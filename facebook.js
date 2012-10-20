7be74d64c814e8bc162da39278123f6248901be5
Dan Peguine <danpeg@gmail.com>
2012-10-20 20:42:24 +0200

    cleanup

--- facebook.js
+++ facebook.js
@@ -1,62 +1,36 @@
 chrome.extension.sendMessage({method: "getLocalStorage"}, function(response) {
-
     // increment to storage
     var score = function() {
-
 	    var d = new Date(); 
 	    if(!localStorage["visitstofacebook"]) localStorage["visitstofacebook"] = 1;
 	    if(!localStorage["lastaccess"]) localStorage["lastaccess"] = d;
-	    var last = new Date(localStorage["lastaccess"]);
 
+	    var last = new Date(localStorage["lastaccess"]);
+	    localStorage["lastaccess"] = d; 
 
 	    if(!(last.getDay() == d.getDay() && 
 	       last.getMonth() == d.getMonth() &&
 	       last.getYear() == d.getYear())){
-		    //not same day
+		    //not the same day, reset counter
 		localStorage["visitstofacebook"] = 1; 
 	    } else { 
 		localStorage["visitstofacebook"]++;
 	    }
 
-
-	    localStorage["lastaccess"] = d; 
-
-
-	    console.log(localStorage);
-
-
 	    notificationDefaults = {
-		    title:'FacebookConditioner', 
-		    time: 0
+		    title:'Facebook DEconditioner', 
+		    time: 3000
 	  };
 
         var notification = jQuery.extend(notificationDefaults, {
-          text: "Visited Facebook " +  localStorage["visitstofacebook"] + " Times today"
+          text: "You visited Facebook " +  localStorage["visitstofacebook"] + " Times today"
         });
         chrome.extension.sendMessage({method: "showNotification", notification: notification}, function(response) {}); 
       }
 
 
-    console.log(window.location.hostname);
     if(window.location.hostname == "www.facebook.com"){
       score();
     }
 
-    /*
-    var viceDomains = "facebook.com";
-    var wwwViceDomains = _.map(viceDomains, function(domain){
-      return 'www.'+domain
-    });
-    var badHosts = viceDomains.concat(wwwViceDomains);
-    if (_.include(badHosts, window.location.hostname)) {
-      // Dock points once they enter the site, and every 5 minutes they're on the site
-      score();
-    }
-    
-    // Give points for completing Workflowy tasks 
-    /*if (window.location.hostname === 'workflowy.com') {
-      jQuery(".editor").watch('text-decoration', function(){
-        console.log(jQuery(this));
-      });
-    }*/
 });
